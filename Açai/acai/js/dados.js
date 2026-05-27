1532990ccf846c775d3a07451592abaf672f898e
zezin226 <josearthuraraujolopes@gmail.com>
2026-05-27 11:30:03 -0300

    Add files via upload

---
+++ Açai/acai/js/dados.js
@@ -0,0 +1,44 @@
+// Dados do cardápio/serviços do cliente.
+// Edite este arquivo para alterar os itens exibidos no site.
+const ITENS = [
+ {
+  nome: "Açaí 300ml",
+  descricao: "Açai de 300ml com acompanhamentos inclusos.",
+  preco: "R$ 15,00"
+ },
+  {
+    nome: "Açaí 500ml",
+    descricao: "Açai de 500ml com acompanhamentos inclusos.",
+    preco: "R$ 20,00"
+  },
+  {
+    nome: "Tigelão 1L",
+    descricao: "Açai de 1l com acompanhamentos inclusos.",
+    preco: "R$ 25,00"
+  },
+  { 
+    nome: "Smoothie",
+    descricao: "Açai batido com frutas a partes(opicional).",
+    preco: "R$ 30,00"
+  },
+  {
+    nome: "Barca de Açai",
+    descricao: "Barca de açai com acompanhamentos inclusos(de sua preferencia).",
+    preco: "R$ 45,00"
+  },
+  {
+    nome: "Creme de Frutas",
+    descricao: "Creme de frutas de sua preferencia(cupuaçu, maracujá, morango, banana).",
+    preco: "R$ 20,00"
+  },
+  { 
+    nome: "Açai zero açúcar", 
+    descricao: "Açai sem adição de açucar, com acompanhamentos inclusos(zero açúcar).", 
+    preco: "R$ 20,00" 
+  },
+  { 
+    nome: "Copos Montados", 
+    descricao: "Copos montados de 300ml, 500ml ou 1l especialidades da casa.", 
+    preco: "R$ 15,00 - R$ 25,00 - R$30,00" 
+  },
+];
